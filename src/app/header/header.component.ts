7823cdc31201a2c234d688bfb0a3160fbcb176d8
somberSomni <kartunehustla@gmail.com>
2019-10-18 16:22:41 -0500

    created navigation using if statments, should use routing later

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed:boolean = true;
+  @Output("changePage") changePage = new EventEmitter<{newPage: string}>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  setPage(newPage: string) {
+    console.log('setting page to : ' + newPage);
+    this.changePage.emit({ newPage });
+  }
 }
