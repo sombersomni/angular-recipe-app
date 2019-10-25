133bc13dbdb07a59989764284f168b61c6d282a5
somberSomni <kartunehustla@gmail.com>
2019-10-25 04:48:37 -0500

    Added an unsubscribe to a observer so no memory leaks occur

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,28 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
 
   recipes: Recipe[];
-
+  recipeChangeSub$: Subscription;
   constructor(private recipeService: RecipeService,
     private router: Router, 
     private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
-    this.recipeService.onRecipeChanges.subscribe((recipes: Recipe[]) => {
+    this.recipeChangeSub$ = this.recipeService.onRecipeChanges.subscribe((recipes: Recipe[]) => {
       this.recipes = recipes;
     })
   }
 
+  ngOnDestroy() {
+    this.recipeChangeSub$.unsubscribe();
+    
+  }
   onCreateRecipe() {
     this.router.navigate(["create"], {relativeTo: this.route});
   }
